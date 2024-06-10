37961c3b1b869f6206a46991c2176b951ae4832c
unknown <yousef-1122@hotmail.com>
2024-06-11 01:10:22 +0300

    Ss

---
+++ Calculator/CalculatorDemo.cs
@@ -0,0 +1,42 @@
+﻿namespace Calculator;
+
+class CalculatorDemo
+{
+    static void Main(string[] args)
+    {
+        int number_1, number_2, result;
+        string operation;
+
+        Console.WriteLine("Enter the first number: ");
+        number_1 = Convert.ToInt32(Console.ReadLine()); //casting string to int
+
+        Console.WriteLine("Enter the second number: ");
+        number_2 = Convert.ToInt32(Console.ReadLine()); //casting string to int
+
+
+        Console.WriteLine("Enter the desired operation: ");
+        Console.WriteLine("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division");
+        operation = Console.ReadLine();
+
+        switch (operation)
+        {
+            case "1":
+                result = number_1 + number_2;
+                break;
+            case "2":
+                result = number_1 - number_2;
+                break;
+            case "3":
+                result = number_1 * number_2;
+                break;
+            case "4":
+                result = number_1 / number_2;
+                break;
+            default:
+                result = 0;
+                Console.WriteLine("Invalid operation");
+                break;
+            }
+            Console.WriteLine("The result is: " + result);
+        }
+    }
