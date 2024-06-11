cb8427044a42728255fb7c2d4d0a27a663f81364
unknown <yousef-1122@hotmail.com>
2024-06-11 04:31:00 +0300

    Turn Based game

---
+++ TurnBasedGame/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace TurnBasedGame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Unit player = new Unit(100, 20, 10, "Player");
+        Unit enemy = new Unit(100, 10, 5, "Enemy");
+
+        Console.WriteLine($"Battle between {player.UnitName} and {enemy.UnitName}");
+        Console.WriteLine("\n ");
+        Console.WriteLine("Press 'a' to attack or 'h' to heal");
+        Battle(player, enemy);
+
+    }
+
+    private static void Battle(Unit player, Unit enemy)
+    {
+        int turn = 0;
+        while (player.Hp > 0 || enemy.Hp > 0)
+        {
+            Console.WriteLine($"\n-------- Turn {++turn} --------");
+            Console.WriteLine($" {player.UnitName} HP: {player.Hp}");
+            Console.WriteLine($" {enemy.UnitName} HP: {enemy.Hp}");
+            
+            PlayerTurn(player, enemy);
+            System.Threading.Thread.Sleep(1000); // 1 second delay
+            EnemyTurn(player, enemy);
+            System.Threading.Thread.Sleep(1000); // 1 second delay
+            
+            if (player.isDead || enemy.isDead){break;}
+        }
+    }
+
+    private static void EnemyTurn(Unit player, Unit enemy)
+    {
+        Console.WriteLine("\n Enemy turn");
+
+        Random random = new Random(); // determen whither the enemy attacks or heal
+        int enemychoice = random.Next(1, 3);
+        
+        if (enemychoice == 1)
+        {
+            enemy.Attack(player);
+        }
+        else if (enemychoice == 2)
+        {
+            enemy.Heal();
+        }
+    }
+
+    private static void PlayerTurn(Unit player, Unit enemy)
+    {
+        Console.WriteLine("\n Player turn");
+        var choice = Console.ReadKey().KeyChar;
+        Console.WriteLine("\n");
+        if (choice == 'a')
+        {
+            player.Attack(enemy);
+        }
+        else if (choice == 'h')
+        {
+            player.Heal();
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
+    }
+}
---
+++ TurnBasedGame/Unit.cs
@@ -0,0 +1,57 @@
+﻿namespace TurnBasedGame;
+
+internal class Unit
+{
+    private string unitName;
+    private int currentHP;
+    private int maxHP;
+    private int attackPower;
+    private int healPower;
+    private Random random;
+
+    public int Hp { get { return currentHP; }}
+    public String UnitName { get { return unitName; }}
+
+    public Boolean isDead { get {return currentHP <= 0;} } // check remaining hp
+
+    public Unit(int maxHP, int attackPower, int healPower, String unitName)
+    {
+        this.maxHP = maxHP;
+        this.currentHP = maxHP; // start game with max hp
+        this.attackPower = attackPower;
+        this.healPower = healPower;
+        this.unitName = unitName;
+        this.random = new Random();
+    }
+
+    public void Attack(Unit target)
+    {
+        double criticalChance = random.NextDouble(); // random number between 0 and 1
+        criticalChance = criticalChance / 2 + 0.75f; // this will make the random number between 0.75 and 1.25
+        
+        // now the attack can be between (75% - 125%) of the Unit attack power
+        int damage = (int)(attackPower * criticalChance);
+        target.TakeDamage(damage);
+        Console.WriteLine($" {unitName} attacks {target.unitName} for {damage} damage");
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+
+        if(isDead){
+            Console.WriteLine($" {unitName} is dead");
+        }
+    }
+    public void Heal(){
+        double rng = random.NextDouble(); // random number between 0 and 1
+        rng = rng / 2 + 0.75f; // this will make the random number between 0.75 and 1.25
+
+        // simmilar to criticalchance, now the heal amount can be between (75% - 125%) of the Unit heal power
+        int heal = (int)(healPower * rng);
+
+        currentHP = heal + currentHP > maxHP ? maxHP : heal + currentHP; // if the heal + currentHP is greater than maxHP, then set currentHP to maxHP
+        Console.WriteLine($"{unitName} heals for {heal} HP");
+    }
+}
